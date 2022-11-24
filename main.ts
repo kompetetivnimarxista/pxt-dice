455d699f95028521a526563c7edda6d7dc9e51fa
Antonín Reiser <115144153+kompetetivnimarxista@users.noreply.github.com>
2022-11-24 11:36:39 +0100

    hotovo

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+
+let number = 0
+whaleysans.showNumber(0)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)){
+        number -= 1
+        whaleysans.showNumber(number)
+        basic.pause(200)
+    }
+    if (input.buttonIsPressed(Button.B)){
+        if (number > 9){
+            number = 9
+        }
+        number += 1
+        whaleysans.showNumber(number)
+        basic.pause(200)
+    }
 })
+
